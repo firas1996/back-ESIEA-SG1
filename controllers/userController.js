0babf515e4f285444bca4627664270aa273ba992
firas1996 <firas.hm.1996@gmail.com>
2025-05-27 11:36:58 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,34 @@ exports.getuserById = async (req,res)=>{
         })
     }
 }
+
+
+exports.updateUser = async (req,res)=>{
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id,req.body)
+        res.status(200).json({
+            message:"User updated !!!",
+            data:{user}
+        })
+    } catch (error) {
+        res.status(400).json({
+            message:"Fail !!",
+            error:error
+        })
+    }
+}
+
+
+exports.deleteUser = async (req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            message:"User deleted !!!",
+        })
+    } catch (error) {
+        res.status(400).json({
+            message:"Fail !!",
+            error:error
+        })
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { createUser, getAllUsers, getuserById } = require('../controllers/userController')
+const { createUser, getAllUsers, getuserById, updateUser, deleteUser } = require('../controllers/userController')
 const router = express.Router()
 
 router.route('/').post(createUser)
 router.route('/').get(getAllUsers)
 router.route('/:id').get(getuserById)
+router.route('/:id').patch(updateUser)
+router.route('/:id').delete(deleteUser)
 
 module.exports = router
